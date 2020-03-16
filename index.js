cea891cf60dfa5c011fb2705d8af9245c466b76e
chandan <chandannick@gmail.com>
2020-03-16 13:37:22 +0530

    created views and called from home_controller

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 const port = 8000;
 
 //Use express router
-
 app.use("/", require("./routes"));
 
-app.listen(port, function(err) {
+// Setup the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running in server : ${err}`);
     }
