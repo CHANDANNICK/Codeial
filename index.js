0f2ad1e4bf05209626b1d49806a2e0cc435c8d5a
chandan <chandannick@gmail.com>
2020-03-16 15:22:19 +0530

    extracted styles and scripts

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 const port = 8000;
 const expressLayouts = require("express-ejs-layouts");
 
+// Using assets to apply css js images
+app.use(express.static("./assets"));
+
 //Use layouts before accessing views
 app.use(expressLayouts);
+
+// extract style and scripts from sub-pages into layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 //Use express router
 app.use("/", require("./routes"));
