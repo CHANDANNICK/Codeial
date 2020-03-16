35983ea9af6e4c2757ca820d56773d370bba882d
chandan <chandannick@gmail.com>
2020-03-16 14:38:54 +0530

    installed express-ejs-layouts and setting up layouts

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require("express-ejs-layouts");
+
+//Use layouts before accessing views
+app.use(expressLayouts);
 
 //Use express router
 app.use("/", require("./routes"));
@@ -9,9 +13,9 @@ app.use("/", require("./routes"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.listen(port, function (err) {
-    if (err) {
-        console.log(`Error in running in server : ${err}`);
-    }
-    console.log(`Server is running on : ${port}`);
+app.listen(port, function(err) {
+  if (err) {
+    console.log(`Error in running in server : ${err}`);
+  }
+  console.log(`Server is running on : ${port}`);
 });
