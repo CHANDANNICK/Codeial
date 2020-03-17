83de2b187b2ff219fb7ef4e31ab5a2755805b9bc
Chandan Nick <chandannick@gmail.com>
2020-03-17 22:22:11 +0530

    created a object to get the sign up data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,10 @@
 module.exports.home = function(req, res) {
+  // reading cookies
+  console.log(req.cookies);
+
+  //changing cookies
+  res.cookie("nick", 25);
+
   return res.render("home", {
     title: "Home"
   });
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/users");
+
 module.exports.profile = function(req, res) {
   return res.render("user_profile", {
     title: "User Page"
@@ -20,7 +22,25 @@ module.exports.signIn = function(req, res) {
 
 // creating a function to get the sign up data
 module.exports.create = function(req, res) {
-  //TODO later
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function(err, user) {
+    if (err) {
+      console.log("Error in finding the user");
+      return;
+    }
+    if (!user) {
+      User.create(req.body, function(err, user) {
+        if (err) {
+          console.log("Error in creating the user");
+        }
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
 };
 
 // sign in and create a session for th user
